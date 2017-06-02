776757d31df8662093419d58ef01c5d1830fa093
ms1 <ms1@vmware.com>
2017-06-02 10:49:22 +0530

    Updated Layout and added imagesource

--- app/src/main/java/com/dice/ms1/bmicalculator/MainActivity.java
+++ app/src/main/java/com/dice/ms1/bmicalculator/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -18,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
         Button calculatebutton = (Button) findViewById(R.id.calculate_button);
         final EditText massvalue = (EditText) findViewById(R.id.mass_text);
         final EditText heightvalue = (EditText) findViewById(R.id.height_text);
+        final ImageView imagedis = (ImageView) findViewById(R.id.imagedisplay);
 
         calculatebutton.setOnClickListener(new View.OnClickListener()
           {
@@ -27,20 +29,24 @@ public class MainActivity extends AppCompatActivity {
                   double mass = Double.parseDouble(massvalue.getText().toString());
                   double height = Double.parseDouble(heightvalue.getText().toString());
                   double bmi = mass/height;
+
                   String weightresult = "";
                   if(bmi > 25)
                   {
                       weightresult = "You are Overweight!!!";
+                      imagedis.setImageResource(R.drawable.overweight);
                   }
                   else if((bmi > 18.5) && (bmi <= 25))
                   {
                       weightresult = "You have Normal weight :)";
+                      imagedis.setImageResource(R.drawable.normal);
                   }
-                  else if(bmi < 18)
+                  else if(bmi < 18.5)
                   {
                       weightresult = "You are Underweight :(";
+                      imagedis.setImageResource(R.drawable.underweight);
                   }
-                 Toast.makeText(MainActivity.this, "BMI value is : "+bmi+" and "+weightresult, Toast.LENGTH_SHORT).show();
+                 Toast.makeText(MainActivity.this, "BMI value is : "+bmi+" and "+weightresult, Toast.LENGTH_LONG).show();
                  }
            }
         );
